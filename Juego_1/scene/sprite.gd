c7ab1b9fd4bc1fc1101438cb8943ee2b1259ecf4
Roberth <41274329+roberthcobena@users.noreply.github.com>
2021-10-20 17:46:15 -0500

    Actualización - aun tengo que aprender más, estudiaré más la documentación oficial

--- Juego_1/scene/sprite.gd
+++ Juego_1/scene/sprite.gd
@@ -1,10 +1,13 @@
 extends Sprite
 
+
 func _process(delta):
  if Input.is_action_pressed("ui_right"):
-  rotation +=1*delta
-  get_node("AudioStreamPlayer").play()
+  rotation +=1*delta 
+  sonido()  
  if Input.is_action_pressed("ui_left"):
   rotation -=1*delta
-  get_node("AudioStreamPlayer").play()
 pass
+
+func sonido():
+ get_tree().get_nodes_in_group("sfx")[0].get_node("rotar").play()
