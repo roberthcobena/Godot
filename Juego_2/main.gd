7ed9eae0273615182dfb04ad98755f34b5c3dee9
Roberth <41274329+roberthcobena@users.noreply.github.com>
2021-10-22 12:04:07 -0500

    Juego base semi-terminado - Faltan correcciones medias y menores

--- Juego_2/main.gd
+++ Juego_2/main.gd
@@ -5,16 +5,25 @@ var score
 
 func _ready():
 	randomize()
-	new_game()
+#	new_game()
 	
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
+	get_tree().call_group("mobs", "queue_free")
+	
+	$Music.stop()
+	$DeathSound.play()
 	
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
-	$StartTimer.start()
+	$StartTimer.start()	
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	
+	$Music.play()
 
 func _on_StartTimer_timeout():
 	$MobTimer.start()
@@ -22,20 +31,21 @@ func _on_StartTimer_timeout():
 
 func _on_ScoreTimer_timeout():
 	score += 1
+	$HUD.update_score()
 
 func _on_MobTimer_timeout():
-	#escoje ubicacion al azar
+	# Choose a random location on Path2D.
 	$MobPath/MobSpawnLocation.offset = randi()
-	#crea una instancia del enemigo y lo añade a la escena
-	var mob = Mob.instace()
+	# Create a Mob instance and add it to the scene.
+	var mob = Mob.instance()
 	add_child(mob)
-	#configura la dirección perpendicular al area de juego
+	# Set the mob's direction perpendicular to the path direction.
 	var direction = $MobPath/MobSpawnLocation.rotation + PI / 2
-	#configurala posicion del enemigo en un sitio al azar
+	# Set the mob's position to a random location.
 	mob.position = $MobPath/MobSpawnLocation.position
-	#añade algunas direcciones al azar
-	mob.direction += rand_range(PI / 4, PI / 4)
+	# Add some randomness to the direction.
+	direction += rand_range(-PI / 4, PI / 4)
 	mob.rotation = direction
-	#configura la velocidad
-	mob.linear_velocity = Vector2(rand_range(mob.min_speed,mob.max_speed),0) 
-	mob.linaer_velocity = mob.linear_velocity.rotated(direction)
+	# Set the velocity (speed & direction).
+	mob.linear_velocity = Vector2(rand_range(mob.min_speed, mob.max_speed), 0)
+	mob.linear_velocity = mob.linear_velocity.rotated(direction)
